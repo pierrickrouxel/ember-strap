46e9c5e2e365fc999250b3c63d4f17146b7330e9
Pierrick Rouxel <pierrick.rouxel@me.com>
2015-09-18 16:38:17 +0200

    offset property instead of topOffset and bottomOffset for affix

--- addon/components/es-affix.js
+++ addon/components/es-affix.js
@@ -10,29 +10,21 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   /**
-  * Pixels to offset from top of screen when calculating position of scroll.
+  * Pixels to offset from screen when calculating position of scroll.
+  * If a single number is provided, the offset will be applied in both top and bottom directions.
+  * To provide a unique, bottom and top offset just provide an object
+  * `offset: { top: 10 }` or `offset: { top: 10, bottom: 5 }`.
+  * Use a function when you need to dynamically calculate an offset.
   *
-  * @property topOffset
-  * @type Number
+  * @property offset
+  * @type Number|function|Object
   * @default 10
   */
-  topOffset: 10,
-
-  /**
-  * Pixels to offset from bottom of screen when calculating position of scroll.
-  *
-  * @property bottomOffset
-  * @type Number
-  * @default 10
-  */
-  bottomOffset: 10,
+  offset: 10,
 
   didInsertElement() {
     this.$().affix({
-      offset: {
-        top: this.get('topOffset'),
-        bottom: this.get('bottomOffset')
-      }
+      offset: this.get('offset')
     });
   }
 });
